31a20bc30a8ab431d53538f6db2447e32a8cad0b
dogmania <hj296914@gmail.com>
2024-08-07 23:22:33 +0900

    :sparkles: Feat: TaskWorker 구현

---
+++ app/src/main/java/com/example/androidcodingtest/worker/TaskWorker.kt
@@ -0,0 +1,77 @@
+package com.example.androidcodingtest.worker
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.datastore.core.DataStore
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.androidcodingtest.TaskList
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.delay
+
+@HiltWorker
+class TaskWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    private val dataStore: DataStore<TaskList>
+) : CoroutineWorker(context, params) {
+
+    override suspend fun doWork(): Result {
+        // 작업 실행
+        val taskId = inputData.getString("task_id") ?: Result.failure()
+        updateTaskStatus(taskId.toString(), "IN_PROGRESS")
+
+        // 5초 동안 대기 (작업 수행)
+        delay(5000)
+
+        // 작업 완료 후 데이터 저장
+        updateTaskStatus(taskId.toString(), "COMPLETED")
+
+        // 알림 전송
+        sendNotification()
+
+        return Result.success()
+    }
+
+    private suspend fun updateTaskStatus(taskId: String, status: String) {
+        dataStore.updateData { taskList ->
+            val updatedTasks = taskList.tasksList.map { task ->
+                if (task.id == taskId) {
+                    task.toBuilder().setStatus(status).build()
+                } else {
+                    task
+                }
+            }
+            taskList.toBuilder().clearTasks().addAllTasks(updatedTasks).build()
+        }
+    }
+
+    private fun sendNotification() {
+        val channelId = "task_notification_channel"
+        val channelName = "Task Notifications"
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+//            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("작업 완료")
+            .setContentText("작업이 성공적으로 완료되었습니다.")
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+}
