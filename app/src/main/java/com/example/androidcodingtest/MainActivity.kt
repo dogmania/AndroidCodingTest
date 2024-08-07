855c77c02fda6f968d17b7213c867889179d5286
dogmania <hj296914@gmail.com>
2024-08-07 15:09:46 +0900

    :sparkles: Feat: NavHostController를 MainScreen에서 선언 후 관리

--- app/src/main/java/com/example/androidcodingtest/MainActivity.kt
+++ app/src/main/java/com/example/androidcodingtest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.androidcodingtest.navigation.NavigationHost
 import com.example.androidcodingtest.ui.theme.AndroidCodingTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidCodingTestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    AndroidCodingTestTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { paddingValue ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = paddingValue.calculateTopPadding(),
+                        bottom = paddingValue.calculateBottomPadding()
+                    )
+            ) {
+                NavigationHost(navHostController = navController)
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidCodingTestTheme {
-        Greeting("Android")
+
     }
 }
