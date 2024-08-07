d48d09fa5fe981177876cce3db00f61faa326992
dogmania <hj296914@gmail.com>
2024-08-07 23:22:49 +0900

    :sparkles: Feat: TaskWorkerFactory 구현

---
+++ app/src/main/java/com/example/androidcodingtest/util/TaskWorkerFactory.kt
@@ -0,0 +1,26 @@
+package com.example.androidcodingtest.util
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.hilt.work.WorkerAssistedFactory
+import androidx.work.ListenableWorker
+import androidx.work.Worker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import javax.inject.Inject
+
+class TaskWorkerFactory @Inject constructor(
+    @SuppressLint("RestrictedApi") private val workerAssistedFactories: Map<Class<out Worker>, @JvmSuppressWildcards WorkerAssistedFactory<out Worker>>
+) : WorkerFactory() {
+    @SuppressLint("RestrictedApi")
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
+        val foundEntry = workerAssistedFactories.entries.find { Class.forName(workerClassName).isAssignableFrom(it.key) }
+        val factoryProvider = foundEntry?.value
+            ?: throw IllegalArgumentException("unknown worker class name: $workerClassName")
+        return factoryProvider.create(appContext, workerParameters)
+    }
+}
