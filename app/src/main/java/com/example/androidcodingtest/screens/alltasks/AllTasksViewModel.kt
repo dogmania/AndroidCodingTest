29eaef57fe5a1b48c51be6646caeeb45129bc588
dogmania <hj296914@gmail.com>
2024-08-09 02:13:31 +0900

    :sparkles: Feat: 전체 작업 목록 조회 ViewModel 구현

--- app/src/main/java/com/example/androidcodingtest/screens/alltasks/AllTasksViewModel.kt
+++ app/src/main/java/com/example/androidcodingtest/screens/alltasks/AllTasksViewModel.kt
@@ -1,11 +1,35 @@
 package com.example.androidcodingtest.screens.alltasks
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.androidcodingtest.Task
+import com.example.androidcodingtest.base.BaseViewModel
+import com.example.androidcodingtest.data.repository.TaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AllTasksViewModel @Inject constructor(
+    private val taskRepository: TaskRepository
+): BaseViewModel<AllTasksPageState>(AllTasksPageState()) {
 
-): ViewModel() {
+    fun getAllTasks() {
+        viewModelScope.launch {
+            taskRepository.getAllTasks().collect {
+                resultResponse(it, ::getAllTasksSuccessHandler)
+            }
+        }
+    }
+
+    private fun getAllTasksSuccessHandler(data: List<Task>) {
+        Log.e("AllTaskList", data.toString())
+        updateState(
+            uiState.value.copy(
+                taskList = data
+            )
+        )
+    }
 }
