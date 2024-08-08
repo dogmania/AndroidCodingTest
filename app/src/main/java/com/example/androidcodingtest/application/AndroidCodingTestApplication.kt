d26e7da6649c3f379bc788f3b870922cb7a5fb46
dogmania <hj296914@gmail.com>
2024-08-08 21:21:13 +0900

    :bug: Fix: WorkerManager 초기화 코드 추가, WorkerFactory 설정 코드 추가

--- app/src/main/java/com/example/androidcodingtest/application/AndroidCodingTestApplication.kt
+++ app/src/main/java/com/example/androidcodingtest/application/AndroidCodingTestApplication.kt
@@ -1,11 +1,26 @@
 package com.example.androidcodingtest.application
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class AndroidCodingTestApplication: Application() {
+class AndroidCodingTestApplication: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
     override fun onCreate() {
         super.onCreate()
+
+        WorkManager.initialize(this, workManagerConfiguration)
     }
 }
