2238e48b3bd81143654de425c9bf9aec2aebcf2d
dogmania <hj296914@gmail.com>
2024-08-08 21:20:11 +0900

    :sparkles: Feat: 작업의 상태 추가 및 작업 완료 아이콘 추가

--- app/src/main/java/com/example/androidcodingtest/worker/TaskWorker.kt
+++ app/src/main/java/com/example/androidcodingtest/worker/TaskWorker.kt
@@ -9,6 +9,7 @@ import androidx.datastore.core.DataStore
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.androidcodingtest.R
 import com.example.androidcodingtest.TaskList
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -18,19 +19,26 @@ import kotlinx.coroutines.delay
 class TaskWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted params: WorkerParameters,
-    private val dataStore: DataStore<TaskList>
+    private val dataStore: DataStore<TaskList>,
 ) : CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
-        // 작업 실행
         val taskId = inputData.getString("task_id") ?: Result.failure()
-        updateTaskStatus(taskId.toString(), "IN_PROGRESS")
 
-        // 5초 동안 대기 (작업 수행)
+        // 5초 동안 대기
+        delay(5000)
+
+        // 대기
+        updateTaskStatus(taskId.toString(), "대기 중")
+
+        // 진행
+        updateTaskStatus(taskId.toString(), "진행 중")
+
+        // 5초 동안 대기
         delay(5000)
 
         // 작업 완료 후 데이터 저장
-        updateTaskStatus(taskId.toString(), "COMPLETED")
+        updateTaskStatus(taskId.toString(), "완료")
 
         // 알림 전송
         sendNotification()
@@ -67,7 +75,7 @@ class TaskWorker @AssistedInject constructor(
         }
 
         val notification = NotificationCompat.Builder(applicationContext, channelId)
-//            .setSmallIcon(R.drawable.ic_notification)
+            .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle("작업 완료")
             .setContentText("작업이 성공적으로 완료되었습니다.")
             .build()
