fb2caa439ebef2e506450ec9bac72227f2b87c78
dogmania <hj296914@gmail.com>
2024-08-09 02:12:50 +0900

    :sparkles: Feat: 전체 작업 목록 조회 메서드 구현

--- app/src/main/java/com/example/androidcodingtest/data/repositoryImpl/TaskRepositoryImpl.kt
+++ app/src/main/java/com/example/androidcodingtest/data/repositoryImpl/TaskRepositoryImpl.kt
@@ -4,6 +4,8 @@ import androidx.datastore.core.DataStore
 import com.example.androidcodingtest.Task
 import com.example.androidcodingtest.TaskList
 import com.example.androidcodingtest.data.repository.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class TaskRepositoryImpl @Inject constructor(
@@ -17,6 +19,12 @@ class TaskRepositoryImpl @Inject constructor(
                 .setStatus("추가됨")
                 .build()
             taskList.toBuilder().addTasks(newTask).build()
+        }
+    }
+
+    override suspend fun getAllTasks(): Flow<List<Task>> {
+        return dataStore.data.map { taskList ->
+            taskList.tasksList
         }
     }
 }
